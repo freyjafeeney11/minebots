8d33c0cdd548b6c212be69b251e2249c0b2a2849
freyjafeeney11 <freyjafeeney@gmail.com>
2023-02-06 21:54:07 -0500

    sensors finished + edited

--- analyze.py
+++ analyze.py
@@ -1,11 +1,11 @@
 import numpy as numpy
 import matplotlib.pyplot
 
-frontLegSensorValues = numpy.load("data/data.npy")
-torsoSensorValues = numpy.load("data/data2.npy")
-matplotlib.pyplot.plot(frontLegSensorValues,  linewidth = 3, label = 'Front Leg Values')
-matplotlib.pyplot.plot(torsoSensorValues,  linewidth = 3, label = 'Torso Values')
+backLegSensorValues = numpy.load("data/data.npy")
+frontLegSensorValues = numpy.load("data/data2.npy")
+matplotlib.pyplot.plot(backLegSensorValues,  linewidth = 2, label = 'Back Leg Values')
+matplotlib.pyplot.plot(frontLegSensorValues,  linewidth = 2, label = 'Front Leg Values')
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
+print(backLegSensorValues)
 print(frontLegSensorValues)
-print(torsoSensorValues)
