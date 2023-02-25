378b09cda24a4fdb918046415b14f31d826b8b9e
freyjafeeney11 <freyjafeeney@gmail.com>
2023-02-25 00:15:48 -0500

    looking for brain file

--- generate.py
+++ generate.py
@@ -10,12 +10,18 @@ def Create_World():
     pyrosim.Send_Cube(name="Demo", pos=[x,y,z] , size=[length,width,height])
     pyrosim.End()
 def Create_Robot():
-    pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
     pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0.5,0,1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
+def Generate_Body(self):
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
     pyrosim.End()
-Create_Robot()
+def Generate_Brain(self):
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.End()
 Create_World()
+Create_Robot()
+Generate_Body()
+Generate_Brain()
